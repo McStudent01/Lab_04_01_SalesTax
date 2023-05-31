e48f601c2ca6e0bd6a3f519a589138b2c1fd250e
McStudent01 <mcmillcy@mail.uc.edu>
2023-05-31 09:55:46 -0500

    finished task 1

--- src/SalesTax.java
+++ src/SalesTax.java
@@ -2,6 +2,10 @@ public class SalesTax
 {
     public static void main(String[] args)
     {
-        
+        double itemPrice = 100;
+        double TAX_RATE = .05;
+        double salesTax = itemPrice * TAX_RATE;
+        System.out.println("Your item price today is " +itemPrice + "and the sales tax is " +salesTax + ".");
+
     }
 }
